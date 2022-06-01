c5894832ac82b229fa8ddbd760c674cf95f8163d
Namountougou <60631581+Namountougou@users.noreply.github.com>
2022-06-01 02:27:49 +0000

    projet fini

--- resources/views/Abonne/create.blade.php
+++ resources/views/Abonne/create.blade.php
@@ -1,42 +1,42 @@
 @extends('App')
 @section('content')
 @if ($errors->any())
-< class="alert alert-danger bg-danger">
+<div class=" bg-danger fs-4 fw-bold text center col-md-6 offset-md-3" style="height: 40px">
 
     @foreach ($errors->all() as $error)
     {{ $error }}
     @endforeach
 
-    </div>
-    @endif
+</div>
+@endif
 
-    <div class="container-fluid d-flex justify-content-center align-items-center  text-center mt-3 ">
-        <div class="row justify-content-center align-items-center rounded-3 " style=" background-color: rgb(220, 217, 213); width: 50%; height: 400px;">
-            <div class=" col-md-3  log">
-                <img src="{{asset('img/Logo.png')}}">
-            </div>
-            <div class=" input-group col-12 d-flex justify-content-center">
-
-                <form action="{{route('Abonne.store')}}" method="POST" class="fw-bold fs-6">
-                    @csrf
-                    @method('POST')
-                    <input type="text" class="form-control border-bottom-1" name="nom" placeholder="nom et prenom">
-                    <input type="date" class="form-control border-bottom-1 mt-1" name="date_n" placeholder="Date de naissance">
-                    <input type="date" class="form-control border-bottom-1 mt-1" name="date_db" placeholder="Date d'abonnement">
-                    <input type="text" class="form-control border-bottom-1 mt-1" name="telephone" placeholder="Telephone">
-                    <input type="email" class="form-control border-bottom-1 mt-1" name="email" placeholder="E-mail">
-            </div>
-            <div class="d-flex justify-content-center">
-                <div class="d-flex justify-content-around ms-4"> <button class="btn btn-primary rounded-pill fw-bold" type="submit" name="valider">Ajouter</button>
-                    <a class="btn btn-danger rounded-pill fw-bold" href="{{route('home')}}">Annuler</a>
-                    <a class="btn btn-success rounded-pill fw-bold" href="{{route('Abonne.index')}}">Lister</a>
-                </div>
-            </div>
-
-            </form>
-
-
-
+<div class="container-fluid d-flex justify-content-center align-items-center  text-center mt-3 ">
+    <div class="row justify-content-center align-items-center rounded-3 " style=" background-color: rgb(220, 217, 213); width: 50%; height: 400px;">
+        <div class=" col-md-3  log">
+            <img src="{{asset('img/Logo.png')}}">
         </div>
+        <div class=" input-group col-12 d-flex justify-content-center">
+
+            <form action="{{route('Abonne.store')}}" method="POST" class="fw-bold fs-6">
+                @csrf
+                @method('POST')
+                <input type="text" class="form-control border-bottom-1" name="nom" placeholder="nom et prenom">
+                <input type="date" class="form-control border-bottom-1 mt-1" name="date_n" placeholder="Date de naissance">
+                <input type="date" class="form-control border-bottom-1 mt-1" name="date_db" placeholder="Date d'abonnement">
+                <input type="text" class="form-control border-bottom-1 mt-1" name="telephone" placeholder="Telephone">
+                <input type="email" class="form-control border-bottom-1 mt-1" name="email" placeholder="E-mail">
+        </div>
+        <div class="d-flex justify-content-center">
+            <div class="d-flex justify-content-around ms-4"> <button class="btn btn-primary rounded-pill fw-bold" type="submit" name="valider">Ajouter</button>
+                <a class="btn btn-danger rounded-pill fw-bold" href="{{route('home')}}">Annuler</a>
+                <a class="btn btn-success rounded-pill fw-bold" href="{{route('Abonne.index')}}">Lister</a>
+            </div>
+        </div>
+
+        </form>
+
+
+
     </div>
-    @endsection
+</div>
+@endsection
--- resources/views/App.blade.php
+++ resources/views/App.blade.php
@@ -22,12 +22,44 @@
 
     <div class="row d-flex col-md-12">
 
-        <div class="col-md-4 offset-md-1 logo">
+        <div class="col-md-3 offset-md-1 logo">
             <img src="{{asset('img/auf.jpg')}}">
         </div>
-        <div class=" col-md-6 offset-md-1  d-flex align-items-center">
+        <div class=" col-md-5 offset-md-1  d-flex align-items-center">
             <span class="fw-bold fs-1" style="color: rgb(75, 2, 143)">Gestion des Abonnés</span>
         </div>
+        <div class="col-md-2 d-flex align-items-center">
+            @guest
+            @if (Route::has('login'))
+
+            <a class="btn btn-primary" href="{{ route('login') }}">{{ __('Login') }}</a>
+
+            @endif
+
+            @if (Route::has('register'))
+
+            <a class="btn btn-success" href="{{ route('register') }}">{{ __('Register') }}</a>
+
+            @endif
+            @else
+
+
+
+            <a class="btn btn-danger"  href="{{ route('logout') }}" onclick="event.preventDefault();
+                                                     document.getElementById('logout-form').submit();">
+                {{ __('Logout') }}
+            </a>
+
+            <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+                @csrf
+            </form>
+        </div>
+
+        @endguest
+
+
+
+    </div>
 
     </div>
 
